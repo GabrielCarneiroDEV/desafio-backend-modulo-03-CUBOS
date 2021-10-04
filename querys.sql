9d3e7bea71effd65f777c74b52d24c6681aec317
Gabriel Carneiro <gabriel29music@gmail.com>
2021-10-04 19:09:54 -0300

    arquivo sql adicionado

---
+++ querys.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE market_cubos;
+
+CREATE TABLE usuarios (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  nome_loja TEXT NOT NULL,
+  email TEXT NOT NULL,
+  senha TEXT NOT NULL
+);
+
+CREATE TABLE produtos (
+  id SERIAL PRIMARY KEY NOT NULL,
+  usuario_id INT REFERENCES usuario(id) NOT NULL,
+  nome TEXT NOT NULL,
+  quantidade INT NOT NULL,
+  categoria TEXT NOT NULL,
+  preco INT NOT NULL,
+  descricao TEXT NOT NULL,
+  imagem TEXT
+);
+
